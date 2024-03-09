920b2cbb42b5270b25220986e435a2f73e8fff1b
archer <chen61306@gmail.com>
2024-03-10 02:29:10 +0800

    P3

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -2,15 +2,42 @@
 pragma solidity ^0.8.0;
 
 /* Problem 1 Interface & Contract */
+/*
+contract StudentV1 {
+    function register() external view returns (uint256) {
+        // TODO: please add your implementaiton here
+        if(gasleft()<8937393460516634439){
+            return 123;
+        }
+        return 1024;
+    }
+}
+*/
 contract StudentV1 {
     // Note: You can declare some state variable
-
+    bool flag=false;
     function register() external returns (uint256) {
         // TODO: please add your implementaiton here
+        if(!flag){
+            flag=true;
+            return 1024;
+        }
+        return 123;
     }
 }
 
 /* Problem 2 Interface & Contract */
+/*
+contract StudentV2 {
+    function register() external view returns (uint256) {
+        // TODO: please add your implementaiton here
+        if(gasleft()<8937393460516634439){
+            return 123;
+        }
+        return 1024;
+    }
+}
+*/
 interface IClassroomV2 {
     function isEnrolled() external view returns (bool);
 }
@@ -18,6 +45,10 @@ interface IClassroomV2 {
 contract StudentV2 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        if(!IClassroomV2(msg.sender).isEnrolled()){
+            return 1024;
+        }
+        return 123;
     }
 }
 
@@ -25,5 +56,9 @@ contract StudentV2 {
 contract StudentV3 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        if(gasleft()<7191){
+            return 123;
+        }
+        return 1024;
     }
 }
